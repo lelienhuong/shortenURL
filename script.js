5028aa287171d1ff2bcca73aaac8595a5173bfe7
lelienhuong <huonglien212@gmail.com>
2020-10-31 20:13:54 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+restart = () => {
+    var x = window.location.href;
+    var y = x;
+    var z;
+    while (y.indexOf("/") !== -1) {
+        z = y.indexOf("/")
+        y = y.replace("/", ".");
+    }
+    z++;
+    var t = x.substr(z, x.length)
+    if (t.length != 0) {
+        // window.location.href = "https://dsc-dut.herokuapp.com/" + t;
+    }
+}
 
-document
-    .getElementById('submit-url')
-    .addEventListener('click', function () {
-        // Gửi url tới backend
+var input = document
+    .getElementById('input-url')
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("submit-url").click();
+    }
+});
+
+checkNull = () => {
+    var inputLink = document.getElementById('input-url').value;
+    inputLink = inputLink.trim()
+    if (inputLink.indexOf(" ") !== -1 || inputLink.length == 0) {
+        alert("Please input your link again!")
+    } else {
         var x = document.getElementById('shorten-output');
         if (x.style.display === 'none') {
             x.style.display = 'block';
@@ -10,47 +35,35 @@ document
             x.style.display = 'none';
             window.location.reload();
         }
-        // axios.post("https://dsc-dut.herokuapp.com/url",{
-        //     url : document.getElementById('input-url').value
-        // })
-        // .then(res=>console.log(res.data))
-        const yourJsonObject = {
-             url : document.getElementById('input-url').value            
-        }
-        var slug ='';
+
+        var slug = '';
         window.fetch("https://dsc-dut.herokuapp.com/url", {
-            method: 'POST', 
-            body: JSON.stringify({"url":document.getElementById('input-url').value}), 
-            headers:{
+            method: 'POST',
+            body: JSON.stringify({ "url": document.getElementById('input-url').value }),
+            headers: {
                 'Content-Type': 'application/json'
             }
-            })
+        })
             .then(res => res.json())
             .then(response => slug = response.slug)
             .then(response => document.getElementById('output-url').value = window.location.href + slug)
             .catch(error => console.error('Error:', error))
-
-        // window.fetch("https://dsc-dut.herokuapp.com/url", { 
-        //     method: 'POST', 
-        //     body: JSON.stringify(yourJsonObject), 
-        //   }).then(res => console.log((res.json())))
-        //   .then(document.getElementById('output-url').value = window.location.href +3)
-        //     .catch(err=>console.log(err));
-        //   .then(res=>console.log(res.dat)); 
-
-          //        alert("xu" + document.getElementById('input-url').value);
-      }
-
-        // document.getElementById('output-url').type = "text"        
-        // document.getElementById('output-url').value = document.getElementById('input-url').value
+    }
+}
+document
+    .getElementById('submit-url')
+    .addEventListener('click', function () {
+        checkNull();
+    }
     )
+    
 function copy() {
-        var copyText = document.getElementById("output-url");
-        copyText.select();
-        copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-        document.execCommand("copy");
-      }
-      
+    var copyText = document.getElementById("output-url");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    document.execCommand("copy");
+}
+
 const axios = {
     post: function (url, body) {
         var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
@@ -59,6 +72,3 @@ const axios = {
     }
 }
 
-// axios.post('https://api-url.dscdut.com', {
-//     url: document.getElementById('input-url').value
-// })
